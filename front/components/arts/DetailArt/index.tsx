19702866bd21b64736dfbf38396dc4a905221932
Junyong Park <dev.junyong@gmail.com>
2022-05-03 11:17:27 +0900

    [Feat] 작품 상세 개발

---
+++ front/components/arts/DetailArt/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DetailArtItem } from './styles';
+
+function DetailArt() {
+  return (
+    <div>
+      <div css={DetailArtItem}>index</div>
+    </div>
+  );
+}
+
+export default DetailArt;
