cabbc37339092f96bfc290949b9164d4dc6af0e9
hhhhhhdong <gusehd520@gmail.com>
2022-05-04 16:18:13 +0900

    Add: 작품리스트 UI, 검색 컴포넌트

--- front/pages/arts/index.tsx
+++ front/pages/arts/index.tsx
@@ -1,29 +1,70 @@
-import { Box, Paper, InputBase, Divider, IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import Page from '../../Layouts/Page';
+import { useState, ChangeEvent } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import Page from 'Layouts/Page';
+import ArtCard from 'components/common/ArtCard';
+import SearchBar from 'components/common/SearchBar';
 
 function ArtList() {
+  const [search, setSearch] = useState<String>('');
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  // 검색 API 호출
+  const handleSearchClick = () => {
+    console.log(search);
+  };
+
   return (
     <Page>
-      <Paper
-        variant="outlined"
-        sx={{
-          p: '2px 4px',
-          display: 'flex',
-          alignItems: 'center',
-          flexGrow: 2,
-        }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="작품 이름을 입력하세요."
-          onChange={() => {}}
+      <Typography variant="h2" mt={5}>
+        Arts List
+      </Typography>
+      <Box width="500px" mt={2} mb={5}>
+        <SearchBar
+          handleSearchChange={handleSearchChange}
+          handleSearchClick={handleSearchClick}
         />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton sx={{ p: '10px' }} onClick={() => {}}>
-          <SearchIcon />
-        </IconButton>
-      </Paper>
+      </Box>
+      <Grid container spacing={6}>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+        <Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
+          <ArtCard />
+        </Grid>
+      </Grid>
     </Page>
   );
 }
