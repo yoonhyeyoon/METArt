eebf9f00b5f5cce70b882520b32e17bf6a90bb6f
JosephNaa <js.pekah@gmail.com>
2022-05-08 21:38:48 +0900

    Add Lobby, Characters

---
+++ unity/Assets/Scripts/Lobby/SelectCharacter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SelectCharacter : MonoBehaviour
+{
+    void Update() {
+        Vector3 Rotation = new Vector3(transform.rotation.eulerAngles[0], 0.2f, transform.rotation.eulerAngles[2]);
+        transform.Rotate(Rotation);
+    }
+}
