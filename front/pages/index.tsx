a205b6d694ee929f4c246b7cdfb27333f6be96e5
hhhhhhdong <gusehd520@gmail.com>
2022-05-10 17:28:15 +0900

    Add: nav바 로그인 연동

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,10 +1,10 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import { useRecoilState } from 'recoil';
 import { userAccountState } from 'recoil/userAccount';
 import Page from 'Layouts/Page';
 import LandingVideo from 'components/landing/LandingVideo';
 import LandingSummary from 'components/landing/LandingSummary';
-import { useEffect } from 'react';
 
 declare global {
   interface Window {
@@ -18,10 +18,12 @@ const Home: NextPage = () => {
 
   const getUserInfo = async () => {
     try {
-      const userAccount = await window.ethereum.request({
+      const account = await window.ethereum.request({
         method: 'eth_accounts',
       });
-      setUserAccount(userAccount);
+      if (account) {
+        setUserAccount(account[0]);
+      }
     } catch (error) {
       console.dir(error);
     }
