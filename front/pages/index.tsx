3579edf8313c22cf5255da581cf2d20f2cc6e871
hhhhhhdong <gusehd520@gmail.com>
2022-05-10 16:43:29 +0900

    Add: 메타마스크 로그인

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,39 @@
 import type { NextPage } from 'next';
+import { useRecoilState } from 'recoil';
+import { userAccountState } from 'recoil/userAccount';
 import Page from 'Layouts/Page';
 import LandingVideo from 'components/landing/LandingVideo';
 import LandingSummary from 'components/landing/LandingSummary';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    ethereum: any;
+    web3: any;
+  }
+}
 
 const Home: NextPage = () => {
+  const [userAccount, setUserAccount] = useRecoilState(userAccountState);
+
+  const getUserInfo = async () => {
+    try {
+      const userAccount = await window.ethereum.request({
+        method: 'eth_accounts',
+      });
+      setUserAccount(userAccount);
+    } catch (error) {
+      console.dir(error);
+    }
+  };
+
+  useEffect(() => {
+    window.ethereum.on('accountsChanged', async () => {
+      getUserInfo();
+    });
+    getUserInfo();
+  }, []);
+
   return (
     <>
       <Page>
