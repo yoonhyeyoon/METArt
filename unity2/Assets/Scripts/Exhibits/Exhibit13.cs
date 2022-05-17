e4494a483dd8ba16d0d1ed04e71038e645ae9986
JosephNaa <js.pekah@gmail.com>
2022-05-18 03:40:20 +0900

    Add exhibit area

---
+++ unity2/Assets/Scripts/Exhibits/Exhibit13.cs
@@ -0,0 +1,20 @@
+public class Exhibit13 : Exhibit
+{
+    void Start()
+    {
+        exhibitId = 9;
+        exhibitName = "FloppyDisk 5.2";
+        year = "1976";
+        producer = "Shugart Technology";
+        content = "13";
+
+        m_MinRotate = -180;
+        m_MaxRotate = 180;
+
+        m_TopRigHeight = 15f;
+        m_MidRigHeight = 5f;
+
+        m_minFOV = 5;
+        m_maxFOV = 20;
+    }
+}
