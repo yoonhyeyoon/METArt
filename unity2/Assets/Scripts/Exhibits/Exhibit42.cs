7b13d29c33414fa8169123b11882acfbb990bf28
kgw012 <kgw012@gmail.com>
2022-05-19 11:34:21 +0900

    Add exhibit info

--- unity2/Assets/Scripts/Exhibits/Exhibit42.cs
+++ unity2/Assets/Scripts/Exhibits/Exhibit42.cs
@@ -2,19 +2,6 @@ public class Exhibit42 : Exhibit
 {
     void Start()
     {
-        exhibitId = 9;
-        exhibitName = "FloppyDisk 5.2";
-        year = "1976";
-        producer = "Shugart Technology";
-        content = "42";
-
-        m_MinRotate = -180;
-        m_MaxRotate = 180;
-
-        m_TopRigHeight = 15f;
-        m_MidRigHeight = 5f;
-
-        m_minFOV = 5;
-        m_maxFOV = 20;
+        GetInfo(42);
     }
 }
