7b2cfb24211d7512dce407c4ff4dce5349a02b55
kgw012 <kgw012@gmail.com>
2022-05-19 12:56:35 +0900

    Add set exhibition scheduler

--- back/src/main/java/com/ssafy/metart/db/repository/SaleRepository.java
+++ back/src/main/java/com/ssafy/metart/db/repository/SaleRepository.java
@@ -1,10 +1,12 @@
 package com.ssafy.metart.db.repository;
 
 import com.ssafy.metart.db.entity.Sale;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     Page<Sale> findAll(Pageable pageable);
+    List<Sale> findAllBySaleYnAndIsCanceledOrderByCreatedAtDesc(Boolean saleYn, Boolean isCanceled);
 }
