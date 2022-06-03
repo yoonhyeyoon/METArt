f7a80aaf8e383cbfdf553144da6738198d11f5f3
yoonhyeyoon <hyeyun3306@gmail.com>
2022-06-03 13:31:14 +0900

    Delete: unused file

--- unity/Assets/Scripts/Lobby/SelectCharacter.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class SelectCharacter : MonoBehaviour
-{
-    void Update() {
-        Vector3 Rotation = new Vector3(transform.rotation.eulerAngles[0], 0.2f, transform.rotation.eulerAngles[2]);
-        transform.Rotate(Rotation);
-    }
-}
